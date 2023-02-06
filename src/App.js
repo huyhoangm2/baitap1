3d888ebc4d543a8040d7030930c8e5610e56bd6d
huyhoangm2 <hoangken760@gmail.com>
2023-02-07 01:14:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Header/>
     </div>
   );
 }
